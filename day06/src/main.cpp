96308afc1322519359336b9d437281038804180d
Stephen Molloy <sdmolloy@gmail.com>
2025-12-23 14:22:01 +0100

    Day6p2

--- day06/src/main.cpp
+++ day06/src/main.cpp
@@ -21,9 +21,20 @@ long find_chain_length(const string& node, const std::unordered_map<string, stri
     return distance;
 }
 
+vector<string> find_chain(const string& node, const std::unordered_map<string, string> links) {
+    vector<string> chain;
+    string current = node;
+    while (links.contains(current)) {
+        current = links.at(current);
+        chain.push_back(current);
+    }
+    return chain;
+}
+
 int main(void) {
     string filename = "data/input.txt";
     // string filename = "data/test.txt";
+    // string filename = "data/test2.txt";
     auto result = read_file(filename);
     if (!result) {
         println("ERROR: Problem reading file");
@@ -40,14 +51,42 @@ int main(void) {
 
         links[key] = val;
     }
+    // for (const auto& val: links) {
+    //     println("{}", val);
+    // }
 
     long part1 = 0;
-    for (const auto& node: links) {
+    for (const auto& node: links)
         part1 += find_chain_length(node.second, links);
-    }
-    assert(part1 == 145250);
+    if (filename == "data/input.txt") assert(part1 == 145250);
 
     long part2 = 0;
+    vector<string> you_2_com = find_chain("YOU", links);
+    vector<string> san_2_com = find_chain("SAN", links);
+
+    println("------------------------------");
+    for (const auto& node: you_2_com)
+        println("{}", node);
+    println("------------------------------");
+    for (const auto& node: san_2_com)
+        println("{}", node);
+    println("------------------------------");
+
+    for (size_t i=0; i<you_2_com.size(); i++) {
+        if (you_2_com.back() != san_2_com.back()) break;
+        you_2_com.pop_back();
+        san_2_com.pop_back();
+    }
+
+    println("------------------------------");
+    for (const auto& node: you_2_com)
+        println("{}", node);
+    println("------------------------------");
+    for (const auto& node: san_2_com)
+        println("{}", node);
+    println("------------------------------");
+
+    part2 = you_2_com.size() + san_2_com.size();
 
     println("Part 1: {}", part1);
     println("Part 2: {}", part2);
