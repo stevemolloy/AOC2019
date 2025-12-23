869a9f208e1aa805ea9752f11d1fcef5280d2bde
Stephen Molloy <sdmolloy@gmail.com>
2025-12-23 14:24:52 +0100

    Tidy up

--- day06/src/main.cpp
+++ day06/src/main.cpp
@@ -9,27 +9,8 @@
 using std::println;
 using std::string;
 
-long find_chain_length(const string& node, const std::unordered_map<string, string>& links) {
-    long distance = 1;
-    string current = node;
-    
-    while (links.contains(current)) {
-        current = links.at(current);
-        distance++;
-    }
-    
-    return distance;
-}
-
-vector<string> find_chain(const string& node, const std::unordered_map<string, string> links) {
-    vector<string> chain;
-    string current = node;
-    while (links.contains(current)) {
-        current = links.at(current);
-        chain.push_back(current);
-    }
-    return chain;
-}
+long find_chain_length(const string& node, const std::unordered_map<string, string>& links);
+vector<string> find_chain(const string& node, const std::unordered_map<string, string> links);
 
 int main(void) {
     string filename = "data/input.txt";
@@ -51,26 +32,14 @@ int main(void) {
 
         links[key] = val;
     }
-    // for (const auto& val: links) {
-    //     println("{}", val);
-    // }
 
     long part1 = 0;
     for (const auto& node: links)
         part1 += find_chain_length(node.second, links);
     if (filename == "data/input.txt") assert(part1 == 145250);
 
-    long part2 = 0;
     vector<string> you_2_com = find_chain("YOU", links);
     vector<string> san_2_com = find_chain("SAN", links);
-
-    println("------------------------------");
-    for (const auto& node: you_2_com)
-        println("{}", node);
-    println("------------------------------");
-    for (const auto& node: san_2_com)
-        println("{}", node);
-    println("------------------------------");
 
     for (size_t i=0; i<you_2_com.size(); i++) {
         if (you_2_com.back() != san_2_com.back()) break;
@@ -78,19 +47,34 @@ int main(void) {
         san_2_com.pop_back();
     }
 
-    println("------------------------------");
-    for (const auto& node: you_2_com)
-        println("{}", node);
-    println("------------------------------");
-    for (const auto& node: san_2_com)
-        println("{}", node);
-    println("------------------------------");
-
-    part2 = you_2_com.size() + san_2_com.size();
+    long part2 = you_2_com.size() + san_2_com.size();
+    if (filename == "data/input.txt") assert(part2 == 274);
 
     println("Part 1: {}", part1);
     println("Part 2: {}", part2);
 
     return 0;
+}
+
+long find_chain_length(const string& node, const std::unordered_map<string, string>& links) {
+    long distance = 1;
+    string current = node;
+    
+    while (links.contains(current)) {
+        current = links.at(current);
+        distance++;
+    }
+    
+    return distance;
+}
+
+vector<string> find_chain(const string& node, const std::unordered_map<string, string> links) {
+    vector<string> chain;
+    string current = node;
+    while (links.contains(current)) {
+        current = links.at(current);
+        chain.push_back(current);
+    }
+    return chain;
 }
 
