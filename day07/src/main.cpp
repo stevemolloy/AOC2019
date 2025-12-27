211f9bbe595f2be01554c2a24e72d2d1165858e3
Stephen Molloy <sdmolloy@gmail.com>
2025-12-27 17:45:16 +0100

    Day7p2

--- day07/src/main.cpp
+++ day07/src/main.cpp
@@ -1,6 +1,4 @@
 #include <algorithm>
-#include <sstream>
-#include <unordered_map>
 #include <cassert>
 #include <string>
 #include <print>
@@ -10,11 +8,15 @@
 using std::println;
 using std::string;
 
+int drive_amp_chain(vector<Machine>& amp_chain, const vector<int>& phases, int input);
+int drive_amp_chain_cont(vector<Machine>& amp_chain, const vector<int>& phases, int input);
+
 int main(void) {
     string filename = "data/input.txt";
     // string filename = "data/test1.txt";
     // string filename = "data/test2.txt";
     // string filename = "data/test3.txt";
+    // string filename = "data/test4.txt";
 
     vector<int> phases = {0, 1, 2, 3, 4};
     vector<vector<int>> phase_list;
@@ -35,6 +37,7 @@ int main(void) {
                 println("{}: No output from this phase_list", i);
                 break;
             }
+            assert(machine.input.size() == 0);
             prev_output = machine.output.back();
         }
         if (prev_output > best_output) best_output = prev_output; 
@@ -43,17 +46,74 @@ int main(void) {
     long part1 = best_output;
     if (filename == "data/input.txt") assert(part1 == 20413);
 
-    vector<Machine> amp_chain;
-    for (size_t i=0; i<5; i++) {
-        Machine machine;
-        machine.load_mem_from_file(filename);
-    }
+    phase_list.clear();
+    phases = {5, 6, 7, 8, 9};
+    do {
+        phase_list.push_back(phases);
+    } while (std::ranges::next_permutation(phases).found);
 
+    filename = "data/test4.txt";
     long part2 = 0;
+    for (const vector<int>& phases: phase_list) {
+        vector<Machine> amp_chain;
+        for (size_t i=0; i<5; i++)
+            amp_chain.emplace_back(filename);
+        int new_input = drive_amp_chain_cont(amp_chain, phases, 0);
+        if (new_input > part2) part2 = new_input;
+    }
+    println("Final value of 'part2' = {}", part2);
+
+    filename = "data/test5.txt";
+    part2 = 0;
+    for (const vector<int>& phases: phase_list) {
+        vector<Machine> amp_chain;
+        for (size_t i=0; i<5; i++)
+            amp_chain.emplace_back(filename);
+        int new_input = drive_amp_chain_cont(amp_chain, phases, 0);
+        if (new_input > part2) part2 = new_input;
+    }
+    println("Final value of 'part2' = {}", part2);
+
+    filename = "data/input.txt";
+    part2 = 0;
+    for (const vector<int>& phases: phase_list) {
+        vector<Machine> amp_chain;
+        for (size_t i=0; i<5; i++)
+            amp_chain.emplace_back(filename);
+        int new_input = drive_amp_chain_cont(amp_chain, phases, 0);
+        if (new_input > part2) part2 = new_input;
+    }
+    println("Final value of 'part2' = {}", part2);
 
     println("Part 1: {}", part1);
     println("Part 2: {}", part2);
 
     return 0;
+}
+
+int drive_amp_chain_cont(vector<Machine>& amp_chain, const vector<int>& phases, int input) {
+    input = drive_amp_chain(amp_chain, phases, input);
+    for (;;) {
+        int new_input = drive_amp_chain(amp_chain, {}, input);
+        if (new_input == input) break;
+        input = new_input;
+    }
+    return input;
+}
+
+int drive_amp_chain(vector<Machine>& amp_chain, const vector<int>& phases, int input) {
+    for (int i=0; Machine& amp: amp_chain) {
+        amp.add_input_val(input);
+        if (phases.size() > 0)
+            amp.add_input_val(phases.at(i));
+        amp.run();
+        if (amp.output.size() == 0) {
+            break;
+        }
+        input = amp.output.front();
+        amp.output.pop_front();
+        i += 1;
+    }
+    return input;
 }
 
