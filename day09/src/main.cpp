a6ef39bca74d7a80f5cdd5e4ac141e3e2850d5b1
Stephen Molloy <sdmolloy@gmail.com>
2025-12-28 17:39:59 +0100

    Day9p2

--- day09/src/main.cpp
+++ day09/src/main.cpp
@@ -10,14 +10,25 @@ using std::string;
 int main(void) {
     string filename = "data/input.txt";
 
-    Machine machine(filename);
-    if (filename == "data/input.txt") machine.add_input_val(1);
-    machine.run();
+    long part1;
 
-    long part1 = machine.output[0];
-    if (filename == "data/input.txt") assert(part1 == 3429606717);
+    {
+        Machine machine(filename);
+        if (filename == "data/input.txt") machine.add_input_val(1);
+        machine.run();
+        part1 = machine.output[0];
+        if (filename == "data/input.txt") assert(part1 == 3429606717);
+    }
 
-    long part2 = 0;
+    long part2;
+    {
+        Machine machine(filename);
+        if (filename == "data/input.txt") machine.add_input_val(2);
+        machine.run();
+        part2 = machine.output[0];
+        if (filename == "data/input.txt") assert(part2 == 33679);
+    }
+
     println("Part 1: {}", part1);
     println("Part 2: {}", part2);
 
