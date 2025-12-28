cd6b69039de8821dc48bebd927d0e5d9a569fbeb
Stephen Molloy <sdmolloy@gmail.com>
2025-12-28 17:05:22 +0100

    Day9p1

---
+++ day09/src/main.cpp
@@ -0,0 +1,29 @@
+#include <cassert>
+#include <string>
+#include <print>
+
+#include "machine.hpp"
+
+using std::println;
+using std::string;
+
+int main(void) {
+    string filename = "data/input.txt";
+    // string filename = "data/test1.txt";
+    // string filename = "data/test2.txt";
+    // string filename = "data/test3.txt";
+
+    Machine machine(filename);
+    if (filename == "data/input.txt") machine.add_input_val(1);
+    machine.run();
+
+    long part1 = machine.output[0];
+    if (filename == "data/input.txt") assert(part1 == 3429606717);
+
+    long part2 = 0;
+    println("Part 1: {}", part1);
+    println("Part 2: {}", part2);
+
+    return 0;
+}
+
